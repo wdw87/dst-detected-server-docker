9fcae04007120341e63606dad06601cb97632b9d
Wudw <694439986@qq.com>
2020-12-02 12:39:55 +0800

    添加了世界配置文件的注释

--- template/Caves/leveldataoverride.lua
+++ template/Caves/leveldataoverride.lua
@@ -9,54 +9,63 @@ return {
   name="洞穴",
   numrandom_set_pieces=0,
   override_level_string=false,
-  overrides={
-    banana="default",
-    bats="default",
-    berrybush="default",
-    boons="default",
-    branching="default",
-    bunnymen="default",
-    cave_ponds="default",
-    cave_spiders="default",
-    cavelight="default",
-    chess="default",
-    disease_delay="default",
-    earthquakes="default",
-    fern="default",
-    fissure="default",
-    flint="default",
-    flower_cave="default",
-    grass="default",
+  overrides = {
+		-- MISC
+		task_set = "default", 			-- "classic", "default", "cave_default"
+		start_location = "default", 	-- "caves", "default", "plus", "darkness"
+		world_size = "default", 		-- "small", "medium", "default", "huge"
+		branching = "default", 			-- "never", "least", "default", "most"
+		loop = "default", 				-- "never", "default", "always"
+		season_start = "default", 		-- "default", "winter", "spring", "summer", "autumnorspring", "winterorsummer", "random"
+		weather = "default", 			-- "never", "rare", "default", "often", "always"
+		earthquakes = "default", 		-- "never", "rare", "default", "often", "always"
+		regrowth = "default", 			-- "veryslow", "slow", "default", "fast", "veryfast"
+		touchstone = "default", 		-- "never", "rare", "default", "often", "always"
+		boons = "default", 				-- "never", "rare", "default", "often", "always"
+		cavelight = "default", 			-- "veryslow", "slow", "default", "fast", "veryfast"
+		disease_delay = "none", 		-- "none", "random", "long", "default", "short"
+    prefabswaps_start = "default", 	-- "classic", "default", "highly random"
     layout_mode="RestrictNodesByKey",
-    lichen="default",
-    liefs="default",
-    loop="default",
-    marshbush="default",
-    monkey="default",
-    mushroom="default",
-    mushtree="default",
-    prefabswaps_start="default",
-    reeds="default",
-    regrowth="default",
     roads="never",
-    rock="default",
-    rocky="default",
-    sapling="default",
-    season_start="default",
-    slurper="default",
-    slurtles="default",
-    start_location="caves",
-    task_set="cave_default",
-    tentacles="default",
-    touchstone="default",
-    trees="default",
-    weather="default",
-    world_size="medium",
-    wormattacks="default",
     wormhole_prefab="tentacle_pillar",
-    wormlights="default",
-    worms="default" 
-  },
+
+		-- RESOURCES
+		grass = "default", 				-- "never", "rare", "default", "often", "always"
+		sapling = "default", 			-- "never", "rare", "default", "often", "always"
+		marshbush = "default", 			-- "never", "rare", "default", "often", "always"
+		reeds = "default", 				-- "never", "rare", "default", "often", "always"
+		trees = "default", 				-- "never", "rare", "default", "often", "always"
+		flint = "default", 				-- "never", "rare", "default", "often", "always"
+		rock = "default", 				-- "never", "rare", "default", "often", "always"
+		mushtree = "default", 			-- "never", "rare", "default", "often", "always"
+		fern = "default", 				-- "never", "rare", "default", "often", "always"
+		flower_cave = "default", 		-- "never", "rare", "default", "often", "always"
+		wormlights = "default", 		-- "never", "rare", "default", "often", "always"
+
+		-- UNPREPARED
+		berrybush = "default", 			-- "never", "rare", "default", "often", "always"
+		mushroom = "default", 			-- "never", "rare", "default", "often", "always"
+		banana = "default", 			-- "never", "rare", "default", "often", "always"
+		lichen = "default", 			-- "never", "rare", "default", "often", "always"
+
+		-- ANIMALS
+		cave_ponds = "default", 		-- "never", "rare", "default", "often", "always"
+		slurper = "default", 			-- "never", "rare", "default", "often", "always"
+		bunnymen = "default", 			-- "never", "rare", "default", "often", "always"
+		slurtles = "default", 			-- "never", "rare", "default", "often", "always"
+		rocky = "default", 				-- "never", "rare", "default", "often", "always"
+		monkey = "default", 			-- "never", "rare", "default", "often", "always"
+
+		-- MONSTERS
+		cave_spiders = "default", 		-- "never", "rare", "default", "often", "always"
+		tentacles = "default", 			-- "never", "rare", "default", "often", "always"
+		chess = "default", 				-- "never", "rare", "default", "often", "always"
+		liefs = "default", 				-- "never", "rare", "default", "often", "always"
+		bats = "default", 				-- "never", "rare", "default", "often", "always"
+		fissure = "default", 			-- "never", "rare", "default", "often", "always"
+		wormattacks = "default", 		-- "never", "rare", "default", "often", "always"
+		worms = "default", 				-- "never", "rare", "default", "often", "always"
+	},
   required_prefabs={ "multiplayer_portal" },
   substitutes={  },
   version=4 
